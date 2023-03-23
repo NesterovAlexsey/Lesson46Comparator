858af22f2e189204f759ea3348e5011ac5f43338
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-23 07:04:35 +0100

    refactor, check and FINISH with Task1

--- src/MainBook.java
+++ src/MainBook.java
@@ -13,14 +13,23 @@ public class MainBook {
 
     List<Book> myBestBook = new ArrayList<>();
     myBestBook.add(new Book("Remark", "Three friends", 200));
-    myBestBook.add(new Book("Stevin Kowi", "7 Effective skills", 300));
+    myBestBook.add(new Book("Stephen Covey", "7 Effective skills", 300));
     myBestBook.add(new Book("Достоевский", "Братья Карамазовы", 680));
     myBestBook.add(new Book("Remark", "No change on the western front", 300));
 
+    System.out.println("Sort by Author(in alphabet):");
     Collections.sort(myBestBook);
+    showBookList(myBestBook);
 
+    System.out.println("Sort by Book name(in alphabet):");
+    myBestBook.sort(new NameBookComparator());
+    showBookList(myBestBook);
+  }
+
+  private static void showBookList(List<Book> myBestBook) {
     for (Book book : myBestBook) {
       System.out.println(book);
     }
+    System.out.println();
   }
 }
