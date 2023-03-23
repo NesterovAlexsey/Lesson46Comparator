3540640a77873ba1280965040b8d01e00b614712
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-23 14:45:56 +0100

    override compare() in NumberOfPagesComparator

--- src/NumberOfPagesComparator.java
+++ src/NumberOfPagesComparator.java
@@ -1,2 +1,12 @@
-public class NumberOfPagesComparator {
+import java.util.Comparator;
+
+public class NumberOfPagesComparator implements Comparator<Book> {
+
+  @Override
+  public int compare(Book a, Book b){
+    if(a.getNumberOfPages() != b.getNumberOfPages()) {
+      return a.getNumberOfPages() - b.getNumberOfPages();
+    }
+    return a.getBookName().compareTo(b.getBookName());
+  }
 }
